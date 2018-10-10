620bc6d4ced18e37819f7630c9b4ec73ba1d4008
Mike-Wil <mcwilson123@gmail.com>
2018-10-09 21:25:40 -0400

    Passport and Mongo almost

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -9,6 +9,25 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  countsByAuthor: function(req, res) {
+    db.Book
+      .aggregate([
+        {
+          $group: {
+            _id: "$author",
+            count: { $sum: 1 }
+          }
+        },
+        {
+          $addFields: {
+            key: "$_id",
+            value: "$count"
+          }
+        }
+      ])
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
